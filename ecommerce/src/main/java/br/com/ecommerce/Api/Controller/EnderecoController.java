c9d13c6db84ca00940bedd6880fd8e7bc626fe4f
Pedro Iglesias <pedro.iglesias@edu.pucrs.br>
2022-10-31 18:11:56 -0300

    final

--- ecommerce/src/main/java/br/com/ecommerce/Api/Controller/EnderecoController.java
+++ ecommerce/src/main/java/br/com/ecommerce/Api/Controller/EnderecoController.java
@@ -1,8 +1,23 @@
 package br.com.ecommerce.Api.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ecommerce.Application.Service.IEnderecoService;
+import br.com.ecommerce.Domain.Entities.EnderecoEntity;
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/endereco")
 public class EnderecoController {
+  private final IEnderecoService service;
+
+  @GetMapping("/{cep}")
+  public EnderecoEntity pesquisaCep(@PathVariable String cep) {
+    return this.service.pesquisaCep(cep);
+  }
 
 }
--- ecommerce/src/main/java/br/com/ecommerce/Api/Controller/PedidoController.java
+++ ecommerce/src/main/java/br/com/ecommerce/Api/Controller/PedidoController.java
@@ -1,8 +1,30 @@
 package br.com.ecommerce.Api.Controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PedidoController {
+import br.com.ecommerce.Application.Service.IPedidoService;
+import br.com.ecommerce.Domain.Entities.PedidoEntity;
+import lombok.RequiredArgsConstructor;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/pedido")
+public class PedidoController {
+  private final IPedidoService service;
+
+  @PostMapping("/{usuarioId}")
+  public PedidoEntity save(@PathVariable Long usuarioId) {
+    return this.service.save(usuarioId);
+  }
+
+  @GetMapping()
+  public List<PedidoEntity> findAll() {
+    return this.service.findAll();
+  }
 }
